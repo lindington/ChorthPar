3eae6315bdc9bd11ffc8153388859b95b4e6c63e
Linda Hagberg <Linda.Hagberg@campus-lmu.de>
2021-03-12 10:15:50 +0100

    "updatig slurmfiles and inputs"

--- 05.SFS/03.SFSsum/01.input/build_gl_boots_alp.R
+++ 05.SFS/03.SFSsum/01.input/build_gl_boots_alp.R
@@ -5,21 +5,21 @@ options(scipen=999)
 t <- 'alp'
 
 #Split taxa pair into taxa 1 and 2
-t1 <- 'gom'
-t2 <- 'tar'
+t1 <- 'tar'
+t2 <- 'gom'
   
 #read in bamlists to get number of individuals
-t1file <- read.csv(paste("../../01.SAF_files/01.input/bamlist_a_gom.txt",sep = ""), header = FALSE)
-t2file <- read.csv(paste("../../01.SAF_files/01.input/bamlist_a_tar.txt",sep = ""), header = FALSE)
+t1file <- read.csv(paste("../../../inputs/bamlist_alp_gom.txt",sep = ""), header = FALSE)
+t2file <- read.csv(paste("../../../inputs/bamlist_alp_tar.txt",sep = ""), header = FALSE)
   
 t1n <- length(t1file$V1)
 t2n <- length(t2file$V1)
   
 #read in filenames of gene SFS
-filelist <- list.files(paste0('../../02.SFS_per_gene/02.output/02.big',t))
+filelist <- list.files(paste0('../../02.sfs/03.output/02.big',t))
   
 #Sum gene SFS into total SFS and clean up.
-all_sfs <- t(sapply(filelist, function(x) scan(paste0('../../02.SFS_per_gene/02.output/02.big',t,'/',x)), simplify = TRUE))
+all_sfs <- t(sapply(filelist, function(x) scan(paste0('../../02.sfs/03.output/02.big',t,'/',x)), simplify = TRUE))
   
 all_sfs <- na.omit(all_sfs)
  
@@ -39,12 +39,12 @@ for (b in 1:100) {
   sample_sfs <- all_sfs[sample(nrow(all_sfs),size=nrow(all_sfs),replace=TRUE),]
   boot_sfs <- colSums(sample_sfs)
     
-  sink(paste0('../03.gl_boot_sfs/02.big',t,'/boot',b,'.sfs'), append = FALSE)
+  sink(paste0('../03.boots/02.big',t,'/boot',b,'.sfs'), append = FALSE)
   cat(paste0((2*t1n)+1,' ',(2*t2n)+1,' unfolded "',t1,'" "',t2,'"'))
   cat('\n')
   sink()
    
-  write(boot_sfs, file = paste0('../03.gl_boot_sfs/02.big',t,'/boot',b,'.sfs'), ncolumns = length(boot_sfs), append = TRUE, sep = " ")
+  write(boot_sfs, file = paste0('../03.boots/02.big',t,'/boot',b,'.sfs'), ncolumns = length(boot_sfs), append = TRUE, sep = " ")
     
 }
 
