510ac687aa2516abbf190a39d9d3308ffffd27ae
lindington <lulu.keks@gmx.de>
2021-04-08 20:30:10 +0200

    better dxy script

--- 07.sumstats/03.dxy/02.scrips/calcDxy.R
+++ 07.sumstats/03.dxy/02.scrips/calcDxy.R
@@ -27,48 +27,65 @@
 
 
 ### Creating an argument parser
-install.packages(optparse)
-library("optparse")
+# install.packages("optparse")
+# library("optparse")
 
-option_list = list(
-  make_option(c("-p","--popA"), type="character",default=NULL,help="path to unzipped mafs file for pop 1",metavar="character"),
-  make_option(c("-q","--popB"), type="character",default=NULL,help="path to unzipped mafs file for pop 2",metavar="character"),
-  make_option(c("-t","--totLen"), type="numeric",default=NULL,help="total sequence length for global per site Dxy estimate [optional]",metavar="numeric")
-)
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
+# option_list = list(
+#   make_option(c("-p","--popA"), type="character",default=NULL,help="path to unzipped mafs file for pop 1",metavar="character"),
+#   make_option(c("-q","--popB"), type="character",default=NULL,help="path to unzipped mafs file for pop 2",metavar="character"),
+#   make_option(c("-t","--totLen"), type="numeric",default=NULL,help="total sequence length for global per site Dxy estimate [optional]",metavar="numeric")
+# )
+# opt_parser = OptionParser(option_list=option_list)
+# opt = parse_args(opt_parser)
 
 ### Troubleshooting input
-if(is.null(opt$popA) | is.null(opt$popB)){
-  print_help(opt_parser)
-  stop("One or more of the mafs paths are missing", call.=FALSE)
-}
+# if(is.null(opt$popA) | is.null(opt$popB)){
+#   print_help(opt_parser)
+#   stop("One or more of the mafs paths are missing", call.=FALSE)
+# }
 
-if(grepl('.gz$',opt$popA) | grepl('.gz$',opt$popB)){
-  print_help(opt_parser)
-  stop("One or more of the mafs is gzipped.", call.=FALSE)
-}
+# if(grepl('.gz$',opt$popA) | grepl('.gz$',opt$popB)){
+#   print_help(opt_parser)
+#   stop("One or more of the mafs is gzipped.", call.=FALSE)
+# }
 
-if(is.null(opt$totLen)){
-  print("Total length not supplied. The output will not be a per site estimate.")
-}
+# if(is.null(opt$totLen)){
+#   print("Total length not supplied. The output will not be a per site estimate.")
+# }
+pops1 <- c("ERY","TAR")
 
-### Reading data in
-allfreqA <- read.table(opt$popA,sep='\t',row.names=NULL, header=T)
-allfreqB <- read.table(opt$popB,sep='\t',row.names=NULL, header=T)
+for (pop1 in pops1){
+  if (pop1 == "ERY") {
+    pop2 <- "PAR"
+    totLen <- 177622
+  } else {
+    pop2 <- "GOM"
+    totLen <- 163091
+    }
+  print(pop1)
+  print(pop2)
+  popA <- as.character(paste0("../../../05.SFS/01.SAF/03.output/saf_",pop1,".mafs.gz"))
+  popB <- as.character(paste0("../../../05.SFS/01.SAF/03.output/saf_",pop2,".mafs.gz"))
+  totLen <- as.numeric(totLen)
 
-### Manipulating the table and print dxy table
-allfreq <- merge(allfreqA, allfreqB, by=c("chromo","position"))
-allfreq <- allfreq[order(allfreq$chromo, allfreq$position),]
-# -> Actual dxy calculation
-allfreq <- transform(allfreq, dxy=(knownEM.x*(1-knownEM.y))+(knownEM.y*(1-knownEM.x)))
-write.table(allfreq[,c("chromo","position","dxy")], file="Dxy_persite.txt",quote=FALSE, row.names=FALSE, sep='\t')
-print('Created Dxy_persite.txt')
 
-### Print global dxy
-print(paste0('Global dxy is: ',sum(allfreq$dxy)))
-if(!is.null(opt$totLen)){
-  print(paste0('Global per site Dxy is: ',sum(allfreq$dxy)/opt$totLen))
+  ### Reading data in
+  allfreqA <- read.table(popA,sep='\t',row.names=NULL, header=T)
+  allfreqB <- read.table(popB,sep='\t',row.names=NULL, header=T)
+
+  ### Manipulating the table and print dxy table
+  allfreq <- merge(allfreqA, allfreqB, by=c("chromo","position"))
+  allfreq <- allfreq[order(allfreq$chromo, allfreq$position),]
+  # -> Actual dxy calculation
+  allfreq <- transform(allfreq, dxy=(knownEM.x*(1-knownEM.y))+(knownEM.y*(1-knownEM.x)))
+  write.table(allfreq[,c("chromo","position","dxy")], file="Dxy_persite.txt",quote=FALSE, row.names=FALSE, sep='\t')
+  print('Created Dxy_persite.txt')
+
+  ### Print global dxy
+  print(paste0('Global dxy is: ',sum(allfreq$dxy)))
+  if(!is.null(totLen)){
+    print(paste0('Global per site Dxy is: ',sum(allfreq$dxy)/totLen))
+  }
 }
 
 
